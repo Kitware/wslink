dafd70271147aa6d49b8f0cd8489bea208de74ba
Alessandro Genova <alessandro.genova@kitware.com>
2024-04-10 17:06:46 -0400

    DO NOT MERGE: enable wslink to have typescript source files

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -24,6 +24,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
